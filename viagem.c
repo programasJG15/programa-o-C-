6a513413aaf9f47d21a64e804931ea1a988bed58
jonathan garcia <jhowteen@outlook.com>
2026-01-04 19:42:53 -0300

    programa-simples-viagem

---
+++ viagem.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main() {
+int dia, result, hora, diah, horav;
+int ("novo commit");
+// condição 
+printf("Digite a quantidade de dias de viagem: ");
+scanf("%d", &dia);
+
+printf("Digite a quantidade de horas de viagem: ");
+scanf("%d", &hora);
+
+//leitura
+scanf("numero de dia é: %d", dia);
+
+scanf("numero de horas é: %d", hora);
+//soma
+diah = dia * 24;
+horav = diah + hora;
+
+printf("Quantidade de horas viajadas: %d", horav);
+
+
+return 0;
+}
